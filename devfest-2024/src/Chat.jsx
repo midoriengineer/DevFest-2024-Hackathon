10b904a7c997ed175fda28ddfd4d5fa2feced9f0
Robin-J-V <robin.j.verma@gmail.com>
2024-02-04 00:58:50 -0500

    commit

--- devfest-2024/src/Chat.jsx
+++ devfest-2024/src/Chat.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 import {
@@ -12,122 +11,121 @@ import {
 import OpenAI from "openai";
 import Header from "./Header.jsx";
 
+function Chat() {
+  //VARIABLES-------------------------------------------------------------------
+  const animal = localStorage.getItem("animal");
 
-  function Chat() {
-    //VARIABLES-------------------------------------------------------------------
-    const openai = new OpenAI({
-      apiKey: process.env.REACT_APP_API_KEY,
-      dangerouslyAllowBrowser: true,
-    });
-    const [typing, setTyping] = useState(false);
-    let splitMessages = [];
-    const [messages, setMessages] = useState([
-      {
-        message: "What did you eat today?",
-        sender: "bot",
-      },
-    ]);
-  
-    //FUNCTIONS-------------------------------------------------------------------
-  
-    async function handleSendMessage(message) {
-      const newMessage = {
-        message: message,
-        sender: "You",
-        direction: "outgoing",
+  const openai = new OpenAI({
+    apiKey: process.env.REACT_APP_API_KEY,
+    dangerouslyAllowBrowser: true,
+  });
+  const [typing, setTyping] = useState(false);
+  let splitMessages = [];
+  const [messages, setMessages] = useState([
+    {
+      message: `Congratulations on adopting ${animal.name} Hi! What did you eat today?`,
+      sender: "NutriPet",
+    },
+  ]);
+
+  //FUNCTIONS-------------------------------------------------------------------
+
+  async function handleSendMessage(message) {
+    const newMessage = {
+      message: message,
+      sender: "You",
+      direction: "outgoing",
+    };
+    const newMessages = [...messages, newMessage];
+
+    setMessages(newMessages);
+    setTyping(true);
+
+    await handleMessage(newMessages);
+  }
+
+  async function handleMessage(chatMessages) {
+    let messagesFromApi = chatMessages.map((messageObject) => {
+      let role = "";
+      if (messageObject.sender === "You") {
+        role = "user";
+      } else {
+        role = "assistant";
+      }
+      return {
+        role: role,
+        content: messageObject.message,
       };
-      const newMessages = [...messages, newMessage];
-  
-      setMessages(newMessages);
-      setTyping(true);
-  
-      await handleMessage(newMessages);
-    }
-  
-    async function handleMessage(chatMessages) {
-      let messagesFromApi = chatMessages.map((messageObject) => {
-        let role = "";
-        if (messageObject.sender === "You") {
-          role = "user";
-        } else {
-          role = "assistant";
-        }
-        return {
-          role: role,
-          content: messageObject.message,
-        };
-      });
-      const name = "Sally"
-      const animal = "turtle";
-      let health = 0
-      const messageToSystem = {
-        role: "system",
-        content: `You have asked me 'What did you eat today?'. 
+    });
+
+    let health = 0;
+    const messageToSystem = {
+      role: "system",
+      content: `You have asked me 'What did you eat today?'. 
         If my response does not answer the question, do not respond. 
         Please tell me that my input is invalid and ask me 'What did you eat today?'. 
 
-        My pet is a ${animal} named ${name}. The current health of the environment is ${health}.
+        My pet is a ${animal.animal} named ${animal.name}. The current health of the environment is ${health}.
 
         Based on what I ate, tell me the positive or negative effect my eating habits had on the environment,
-        and how the effected environment affected my ${animal} in the form of a short story in the past tense.
+        and how the effected environment affected my ${animal.animal} in the form of a short story in the past tense.
         Please describe a extremely specific scenario rather than using general language. This story is only to be 1-2 sentences long.
 
 
 
-        If ${health} is less than 3, there is a random chance that my ${animal} will die.
+        If ${health} is less than 3, there is a random chance that my ${animal.animal} will die.
         `,
-      };
-  
-      const requestToApi = {
-        model: "gpt-3.5-turbo",
-        messages: [messageToSystem, ...messagesFromApi],
-      };
-      await fetch("https://api.openai.com/v1/chat/completions", {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${openai.apiKey}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(requestToApi),
+    };
+
+    const requestToApi = {
+      model: "gpt-3.5-turbo",
+      messages: [messageToSystem, ...messagesFromApi],
+    };
+    await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${openai.apiKey}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestToApi),
+    })
+      .then((res) => {
+        return res.json();
       })
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          console.log(data);
-          splitMessages = data.choices[0].message.content.split("\n");
-        });
-      for (const message of splitMessages) {
-        await printMessages(message);
-      }
-      setTyping(false);
-      splitMessages = [];
-    }
-  
-    async function printMessages(message) {
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          const newMessage = {
-            message: message,
-            sender: "bot",
-            direction: "incoming",
-          };
-  
-          if (message.length > 0) {
-            setMessages((chatMessages) => [...chatMessages, newMessage]);
-          }
-          resolve();
-        }, 1000);
+      .then((data) => {
+        console.log(data);
+        splitMessages = data.choices[0].message.content.split("\n");
       });
+    for (const message of splitMessages) {
+      await printMessages(message);
     }
-  
-    return (
-        <>
-        <Header/>
+    setTyping(false);
+    splitMessages = [];
+  }
 
-        <br></br>
+  async function printMessages(message) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const newMessage = {
+          message: message,
+          sender: "bot",
+          direction: "incoming",
+        };
 
-        
+        if (message.length > 0) {
+          setMessages((chatMessages) => [...chatMessages, newMessage]);
+        }
+        resolve();
+      }, 1000);
+    });
+  }
+
+  return (
+    <>
+      <Header />
+
+      <br></br>
+
       <div
         style={{
           position: "relative",
@@ -155,7 +153,7 @@ import Header from "./Header.jsx";
           </ChatContainer>
         </MainContainer>
       </div>
-      </>
-    );
-  }export default Chat;
-  
+    </>
+  );
+}
+export default Chat;
--- devfest-2024/src/Shop.jsx
+++ devfest-2024/src/Shop.jsx
@@ -12,119 +12,144 @@ function Shop() {
   // Define a function that handles the adoption of an animal
   const handleAdopt = (animal) => {
     // Perform some actions, such as adding the animal to the cart, updating the state, or navigating to another page
+    localStorage.setItem("animal", animal);
     console.log(`You have adopted a ${animal}!`);
   };
 
   return (
-<>
-    <Header/>
+    <>
+      <Header />
       <br></br>
 
       <div className="background-container">
-      <br></br>
-
-      <br></br>
-    <Container className="content-container">
-<h1 className="title" style={{fontWeight: "500"}}>Choose a Pet:</h1>
-
-      <Row className="justify-content-md-center">
-        <Col xs={12} md={4}>
-          <Card>
-            <Image
-              src={penguinImage}
-              alt="Penguin"
-              style={{ height: 300, width: 300 }}
-              rounded
-            />
-
-            <Card.Body>
-              <Card.Title><h1 className="title">Oscar the Penguin</h1></Card.Title>
-              <Card.Text>
-                Penguins are flightless birds that live in the Southern
-                Hemisphere. They are excellent swimmers and feed on fish and
-                krill. They are social and live in large colonies.
-              </Card.Text>
-
-              <Button
-              className="shop-button"
-                variant="primary"
-                size="lg"
-                onClick={() => handleAdopt("penguin")}
-              >
-                Adopt me!
-              </Button>
-            </Card.Body>
-          </Card>
-        </Col>
-
-        <br></br>
-        <br></br>
-        <hr></hr>
-
-        <Col xs={12} md={4}>
-          <Card>
-            <Image
-              src={turtleImage}
-              alt="Turtle"
-              style={{ height: 300, width: 300 }}
-              rounded
-            />
-            <Card.Body>
-              <Card.Title><h1 className="title">Sally The Turtle</h1></Card.Title>
-              <Card.Text>
-                Turtles are reptiles that have a hard shell to protect them from
-                predators. They can live in freshwater, saltwater, or on land.
-                They are omnivorous and eat plants and animals.
-              </Card.Text>
-              <Button
-              className="shop-button"
-                variant="primary"
-                size="lg"
-                onClick={() => handleAdopt("turtle")}
-              >
-                Adopt me!
-              </Button>
-            </Card.Body>
-          </Card>
-        </Col>
-
-        <br></br>
         <br></br>
 
-<hr></hr>
-        <Col xs={12} md={4}>
-          <Card>
-            <Image
-              src={bunnyImage}
-              alt="Bunny"
-              style={{ height: 300, width: 300 }}
-              rounded
-            />
-            <Card.Body>
-              <Card.Title><h1 className="title">Kuromi the Bunny</h1></Card.Title>
-              <Card.Text>
-                Bunnies are small mammals that belong to the order Lagomorpha.
-                They have long ears, fluffy tails, and strong hind legs. They
-                are herbivorous and eat grass, hay, and vegetables.
-              </Card.Text>
-              <Button
-              className="shop-button"
-                variant="primary"
-                size="lg"
-                onClick={() => handleAdopt("bunny")}
-              >
-                Adopt me!
-              </Button>
-            </Card.Body>
-          </Card>
-          <br></br>
         <br></br>
-        </Col>
-      </Row>
-    </Container>
-    <br></br>
-    <br></br>
-    </div>
+        <Container className="content-container">
+          <h1 className="title" style={{ fontWeight: "500" }}>
+            Choose a Pet:
+          </h1>
+
+          <Row className="justify-content-md-center">
+            <Col xs={12} md={4}>
+              <Card>
+                <Image
+                  src={penguinImage}
+                  alt="Penguin"
+                  style={{ height: 300, width: 300 }}
+                  rounded
+                />
+
+                <Card.Body>
+                  <Card.Title>
+                    <h1 className="title">Oscar the Penguin</h1>
+                  </Card.Title>
+                  <Card.Text>
+                    Penguins are flightless birds that live in the Southern
+                    Hemisphere. They are excellent swimmers and feed on fish and
+                    krill. They are social and live in large colonies.
+                  </Card.Text>
+
+                  <Button
+                    className="shop-button"
+                    variant="primary"
+                    size="lg"
+                    onClick={() =>
+                      handleAdopt({
+                        animal: "penguin",
+                        name: "Oscar",
+                      })
+                    }
+                  >
+                    Adopt me!
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <br></br>
+            <br></br>
+            <hr></hr>
+
+            <Col xs={12} md={4}>
+              <Card>
+                <Image
+                  src={turtleImage}
+                  alt="Turtle"
+                  style={{ height: 300, width: 300 }}
+                  rounded
+                />
+                <Card.Body>
+                  <Card.Title>
+                    <h1 className="title">Sally The Turtle</h1>
+                  </Card.Title>
+                  <Card.Text>
+                    Turtles are reptiles that have a hard shell to protect them
+                    from predators. They can live in freshwater, saltwater, or
+                    on land. They are omnivorous and eat plants and animals.
+                  </Card.Text>
+                  <Button
+                    className="shop-button"
+                    variant="primary"
+                    size="lg"
+                    onClick={() =>
+                      handleAdopt({
+                        animal: "turtle",
+                        name: "Sally",
+                      })
+                    }
+                  >
+                    Adopt me!
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <br></br>
+            <br></br>
+
+            <hr></hr>
+            <Col xs={12} md={4}>
+              <Card>
+                <Image
+                  src={bunnyImage}
+                  alt="Bunny"
+                  style={{ height: 300, width: 300 }}
+                  rounded
+                />
+                <Card.Body>
+                  <Card.Title>
+                    <h1 className="title">Kuromi the Bunny</h1>
+                  </Card.Title>
+                  <Card.Text>
+                    Bunnies are small mammals that belong to the order
+                    Lagomorpha. They have long ears, fluffy tails, and strong
+                    hind legs. They are herbivorous and eat grass, hay, and
+                    vegetables.
+                  </Card.Text>
+                  <Button
+                    className="shop-button"
+                    variant="primary"
+                    size="lg"
+                    onClick={() =>
+                      handleAdopt({
+                        animal: "bunny",
+                        name: "Kuromi",
+                      })
+                    }
+                  >
+                    Adopt me!
+                  </Button>
+                </Card.Body>
+              </Card>
+              <br></br>
+              <br></br>
+            </Col>
+          </Row>
+        </Container>
+        <br></br>
+        <br></br>
+      </div>
     </>
   );
 }
